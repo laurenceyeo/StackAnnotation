1b1a05873d490fe7a3c249a11f1efc847d31f06f
laurenceyeo <35883723+laurenceyeo@users.noreply.github.com>
2021-11-17 13:34:43 +0800

    Update MyStringStackTest.java

--- test/test/MyStringStackTest.java
+++ test/test/MyStringStackTest.java
@@ -36,8 +36,8 @@ public class MyStringStackTest extends TestCase {
 		stack.push (s1);
 		assertTrue (stack.isEmpty());
 		stack.push (s1);
-        stack.push (s2);
-        assertFalse (stack.isEmpty());
+        	stack.push (s2);
+        	assertFalse (stack.isEmpty());
 	}
 
 	@Test
